5b249a501f262cdadd4f09e1a6f8ee35ae905d2f
Megan Taylor <megan.darlene.taylor@gmail.com>
2020-05-10 22:51:54 -0700

    Final

--- my_services/src/service_client.py
+++ my_services/src/service_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from my_services.rico_srv import WordCount
+from my_services.srv import WordCount
 import sys
 
 rospy.init_node('service_client')
@@ -10,7 +10,9 @@ word_counter = rospy.ServiceProxy( 		# set up proxy
 	'word_count',  #service name
 	WordCount 		# service type
 )
-words = ' '.join(sys.argv[1:]) 	# parse args
+words_list = [k for k in sys.argv if 'service_client.py' not in k]
+words_list = [k for k in sys.argv[1:] if '__' not in k]
+words = ' '.join(words_list) 	# parse args
 word_count = word_counter(words) # use services
 
-print(words+'--> has '+str(word_counter.count)+' words')
+print(words+'--> has '+str(word_count.count)+' words')
--- my_services/src/service_server.py
+++ my_services/src/service_server.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python
+#!/usr/bin/env python
 import rospy
 from my_services.srv import WordCount, WordCountResponse
 
